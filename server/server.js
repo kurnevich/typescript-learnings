69c89baf3f5d687a0c4fc05a73c168de0e170600
Sergei Kurnevich <kurnevitch@gmail.com>
2020-01-07 23:37:10 +0500

    Small refactoring, fixed imports and variable types

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+"use strict";
+// Something is wrong with linter configuration, so some rules are manually disabled
+/* eslint-disable no-undef, @typescript-eslint/camelcase*/
+exports.__esModule = true;
+// Handbook - Interfaces
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
@@ -12,36 +17,14 @@ app.use(cors());
 var router = express.Router();
 var sessions = {};
 // Connect to DB
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", function () { return console.log("connected to the database"); });
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
-// two default users
-// const crypto = require('crypto');
-// function hashCode(str) {
-//   return str.split('').reduce((prevHash, currVal) =>
-//     (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
-// }
-// const admin = new User({
-//     id: 0,
-//     name: 'Admin',
-//     role: 'admin',
-//     password: crypto.createHash('sha256').update(''+hashCode('qweiop')).digest('base64')
-//   });
-// admin.save();
-// const user = new User({
-//     id: 1,
-//     name: 'Salesman',
-//     role: 'salesman',
-//     password: crypto.createHash('sha256').update(''+hashCode('qwe')).digest('base64')
-//   });
-// user.save();
-var cleanupSessions = function () {
+function cleanupSessions() {
     var ses = Object.keys(sessions);
     ses.map(function (id) {
         if (sessions[id] < Date.now()) {
@@ -50,9 +33,50 @@ var cleanupSessions = function () {
             console.log(sessions);
         }
     });
-};
-var sesCleanupInterval = setInterval(cleanupSessions, 10000);
-// How to return proper http error? no such user
+}
+setInterval(cleanupSessions, 10000);
+// Router methods
+router.post("/putData", function (req, res) {
+    var data = new Data();
+    var _a = req.body, id = _a.id, message = _a.message;
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: "INVALID INPUTS"
+        });
+    }
+    data.message = message;
+    data.id = id;
+    data.save(function (err) {
+        if (err)
+            return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
+});
+router.get("/getData", function (req, res) {
+    Data.find(function (err, data) {
+        if (err)
+            return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
+});
+router.post("/updateData", function (req, res) {
+    var _a = req.body, id = _a.id, update = _a.update;
+    Data.findOneAndUpdate(id, update, function (err) {
+        if (err)
+            return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
+});
+router["delete"]("/deleteData", function (req, res) {
+    var id = req.body.id;
+    Data.findOneAndDelete(id, function (err) {
+        if (err)
+            return res.send(err);
+        return res.json({ success: true });
+    });
+});
+// Store example methods
 router.post("/flowers/login", function (req, res) {
     var _a = req.body, user_name = _a.user_name, password = _a.password;
     User.findOne({ name: user_name }, function (err, user) {
@@ -77,55 +101,6 @@ router.post("/flowers/checkSession", function (req, res) {
     }
     return res.json({ auth: false });
 });
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", function (req, res) {
-    Data.find(function (err, data) {
-        if (err)
-            return res.json({ success: false, error: err });
-        return res.json({ success: true, data: data });
-    });
-});
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", function (req, res) {
-    var _a = req.body, id = _a.id, update = _a.update;
-    Data.findOneAndUpdate(id, update, function (err) {
-        if (err)
-            return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
-});
-// this is our delete method
-// this method removes existing data in our database
-router["delete"]("/deleteData", function (req, res) {
-    var id = req.body.id;
-    Data.findOneAndDelete(id, function (err) {
-        if (err)
-            return res.send(err);
-        return res.json({ success: true });
-    });
-});
-// this is our create methid
-// this method adds new data in our database
-router.post("/putData", function (req, res) {
-    var data = new Data();
-    var _a = req.body, id = _a.id, message = _a.message;
-    if ((!id && id !== 0) || !message) {
-        return res.json({
-            success: false,
-            error: "INVALID INPUTS"
-        });
-    }
-    data.message = message;
-    data.id = id;
-    data.save(function (err) {
-        if (err)
-            return res.json({ success: false, error: err });
-        return res.json({ success: true });
-    });
-});
 // append /api for our http requests
 app.use("/api", router);
-// launch our backend into a port
 app.listen(API_PORT, function () { return console.log("LISTENING ON PORT " + API_PORT); });
--- server/server.ts
+++ server/server.ts
@@ -1,33 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const logger = require("morgan");
-const Data = require("./config/db");
-const User = require("./config/db");
-const dbRoute = require("./config/local-config");
+// Something is wrong with linter configuration, so some rules are manually disabled
+/* eslint-disable no-undef, @typescript-eslint/camelcase*/
+
+// Handbook - Interfaces
+
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as logger from 'morgan';
+import * as Data from './config/db';
+import * as User from './config/db';
+import * as dbRoute from './config/local-config';
 
 const API_PORT = 3001;
 const app = express();
 app.use(cors());
 const router = express.Router();
-
 const sessions = {};
 
 // Connect to DB
 
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => console.log("connected to the database"));
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-const cleanupSessions = () => {
+function cleanupSessions(): void {
   const ses = Object.keys(sessions);
   ses.map(id => {
     if (sessions[id] < Date.now()) {
@@ -37,71 +39,13 @@ const cleanupSessions = () => {
     }
   });
 }
+setInterval(cleanupSessions, 10000);
 
-const sesCleanupInterval = setInterval(cleanupSessions, 10000);
+// Router methods
 
-// How to return proper http error? no such user
-router.post("/flowers/login", (req, res) => {
-  const { user_name, password } = req.body;
-  User.findOne({ name: user_name }, function (err, user) {
-    if (err) return console.error(err);
-    if (!user) {
-      return res.json({ error: 'No such user' });
-    }
-    const session = user.makeSession(user_name);
-    sessions[''+session] = Date.now() + 60000;
-    console.log(sessions);
-    return res.json({ auth: user.isPasswordValid(password),
-                      admin: user.isAdmin(),
-                      sessionid: session });
-  });
-});
-
-
-router.post("/flowers/checkSession", (req, res) => {
-  const { id } = req.body;
-  if (sessions[id]) {
-    sessions[id] = Date.now() + 60000;
-    return res.json({auth: true});
-  }
-  return res.json({auth: false});
-});
-
-
-// this is our get method
-// this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findOneAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findOneAndDelete(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-// this is our create method
-// this method adds new data in our database
 router.post("/putData", (req, res) => {
-  let data = new Data();
-  const { id, message } = req.body;
+  const data = new Data();
+  const { id, message }: { id: number; message: string } = req.body;
 
   if ((!id && id !== 0) || !message) {
     return res.json({
@@ -117,8 +61,56 @@ router.post("/putData", (req, res) => {
   });
 });
 
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+router.post("/updateData", (req, res) => {
+  const { id, update }: { id: number; update: string } = req.body;
+  Data.findOneAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.delete("/deleteData", (req, res) => {
+  const { id }: {id: number} = req.body;
+  Data.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+// Store example methods
+
+router.post("/flowers/login", (req, res) => {
+  const { user_name, password }: { user_name: string; password: string } = req.body;
+  User.findOne({ name: user_name }, function (err, user) {
+    if (err) return console.error(err);
+    if (!user) {
+      return res.json({ error: 'No such user' });
+    }
+    const session = user.makeSession(user_name);
+    sessions[''+session] = Date.now() + 60000;
+    console.log(sessions);
+    return res.json({ auth: user.isPasswordValid(password),
+                      admin: user.isAdmin(),
+                      sessionid: session });
+  });
+});
+
+router.post("/flowers/checkSession", (req, res) => {
+  const { id }: {id: number} = req.body;
+  if (sessions[id]) {
+    sessions[id] = Date.now() + 60000;
+    return res.json({auth: true});
+  }
+  return res.json({auth: false});
+});
+
 // append /api for our http requests
 app.use("/api", router);
-
-// launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
